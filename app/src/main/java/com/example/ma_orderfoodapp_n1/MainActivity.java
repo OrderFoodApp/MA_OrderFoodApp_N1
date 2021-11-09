21e605a0ae8d71ac2614f92c194a2d95250395a4
hung2504 <vanhung25042000@gmail.com>
2021-11-09 13:37:22 +0700

    Test

--- app/src/main/java/com/example/ma_orderfoodapp_n1/MainActivity.java
+++ app/src/main/java/com/example/ma_orderfoodapp_n1/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +12,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-// Configure sign-in to request the user's ID, email address, and basic
-// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
     }
 }
